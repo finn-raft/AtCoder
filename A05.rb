e997cf0cccae3becdb61dbe303fd0b2742d7f54f
finn-raft <lemio3g9k@gmail.com>
2024-05-02 16:43:03 +0900

    Three Cards

---
+++ A05.rb
@@ -0,0 +1,18 @@
+n, k = gets.split.map(&:to_i) # 1行で「10 20」のような入力
+
+x = 1
+ansewr = 0
+
+while x <= n do
+    y = 1
+    while y <= n do
+        z = k - x - y
+    if z >= 1 && z <= n
+        ansewr += 1
+    end
+    y += 1
+  end
+  x += 1
+end
+
+puts ansewr
