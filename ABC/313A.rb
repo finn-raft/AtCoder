f5455d68c23ed186b5e3104f55cae58dd47591be
finn-raft <lemio3g9k@gmail.com>
2024-07-11 12:28:56 +0900

    ABC313

--- ABC/313A.rb
+++ ABC/313A.rb
@@ -1,8 +1,12 @@
 n = gets.to_i
 p = gets.split.map(&:to_i)
 
-if p.max >= p[0]
-    puts p.max - p[0] + 1
+if p[0] == p.max
+    if p.count(p.max) > 1
+      puts 1
+    else
+      puts 0
+    end
 else
-    puts 0
+    puts p.max - p[0] + 1
 end
