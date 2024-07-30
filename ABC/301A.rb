afd85d3d3e0a40a9f59c31a8dfe447cb9ecf3aad
finn-raft <lemio3g9k@gmail.com>
2024-07-30 22:03:26 +0900

    ABC301

--- ABC/301A.rb
+++ ABC/301A.rb
@@ -1,23 +1,21 @@
 n = gets.to_i
-s = gets.chomp
+s = gets.chomp.chars
 
-i = 0
 takahashi = 0
 aoki = 0
+winner = ""
 
-shiai = s.chars
-
-while i < n do
-    if shiai[i] == "T"
-        takahashi += 1
-    elsif shiai[i] == "A"
-        aoki +=1
-    end
-    i += 1
+s.each do |match|
+  if match == "T"
+    takahashi += 1
+  else
+    aoki += 1
+  end
+  if takahashi > aoki
+    winner = "T"
+  elsif aoki > takahashi
+    winner = "A"
+  end
 end
 
-if (takahashi > aoki) || takahashi == aoki && shiai[i - 2] == "T"
-    puts "T"
-elsif (aoki > takahashi) || aoki == takahashi && shiai[i - 2] == "A"
-    puts "A"
-end
+puts winner
