1dfeb750684505ca9784c02a843341c7142a4a8f
finn-raft <lemio3g9k@gmail.com>
2024-08-07 00:02:07 +0900

    ABC295

---
+++ ABC/295A.rb
@@ -0,0 +1,19 @@
+n = gets.to_i
+w = gets.split.map(&:to_s)
+
+i = 0
+answer = false
+
+while i < n do
+    if w[i] == "and" || w[i] == "not" || w[i] == "that" || w[i] == "ther" || w[i] == "you"
+        answer = true
+        break
+    end
+    i += 1
+end
+
+if answer
+    puts "Yes"
+else
+    puts "No"
+end
