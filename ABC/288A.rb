851098e8c21c0d415859c0df0e51bcbe9425b9f3
finn-raft <lemio3g9k@gmail.com>
2024-08-18 11:56:41 +0900

    ABC288

---
+++ ABC/288A.rb
@@ -0,0 +1,16 @@
+a ,b ,c = gets.split.map(&:to_i)
+
+if b < c
+  if b < a && a < c
+    puts "No"
+  else
+    puts "Yes"
+end
+
+else
+  if c < a && a < b
+    puts "Yes"
+  else
+    puts "No"
+  end
+end
