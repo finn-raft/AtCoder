9c7831773d2b936062f540ebff98109c9386903f
finn-raft <lemio3g9k@gmail.com>
2024-09-09 01:32:43 +0900

    ABC275

--- ABC/275A.rb
+++ ABC/275A.rb
@@ -3,12 +3,14 @@ h = gets.split.map(&:to_i)
 
 i = 0
 high = 0
+index = 0
 
 while i < n do
     if h[i] > high
-        high = i + 1
+        high = h[i]
+        index = i + 1
     end
     i += 1
 end
 
-puts high
+puts index
