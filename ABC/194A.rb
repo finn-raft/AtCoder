9c7776a3564ceaf9dd4c875e79b6328253f6aaa5
finn-raft <lemio3g9k@gmail.com>
2025-01-04 00:00:11 +0900

    ABC194

--- ABC/194A.rb
+++ ABC/194A.rb
@@ -1,10 +1,10 @@
 a,b = gets.split.map(&:to_i)
 
-if a >= 15 && b >= 8
+if a + b >= 15 && b >= 8
     puts 1
-elsif a >= 10 && b >= 3
+elsif a+ b >= 10 && b >= 3
     puts 2
-elsif a >= 3
+elsif a + b >= 3
     puts 3
 else
     puts 4
