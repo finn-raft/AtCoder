43bd5ce2bd66a3ebf43a8b3ead365cae8db28c45
finn-raft <lemio3g9k@gmail.com>
2025-06-20 06:03:46 +0900

    ABC A

---
+++ ABC/A406.java
@@ -0,0 +1,23 @@
+package ABC;
+
+import java.util.Scanner;
+
+public class A406 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (a > c) {
+            System.out.println("Yes");
+        }
+        else if (a >= c && b > d) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
